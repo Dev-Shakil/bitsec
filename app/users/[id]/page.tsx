05fc36a5bbe33c4239f65e301d128b16b7b9523c
Dev-Shakil <shakilhossain1007@gmail.com>
2025-09-03 23:05:53 +0600

    resolve params issue

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,12 +1,22 @@
-
 import UserDetails from "@/components/UserDetails"
 import { notFound } from "next/navigation"
 
-export default async function UserDetailsPage({ params }: { params: { id: string } }) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`, { cache: "no-store" })
+interface Params {
+  id: string
+}
+
+interface PageProps {
+  params: Params
+}
+
+export default async function UserDetailsPage({ params }: PageProps) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`, {
+    cache: "no-store",
+  })
   if (!res.ok) notFound()
 
   const user = await res.json()
 
   return <UserDetails user={user} />
 }
+
